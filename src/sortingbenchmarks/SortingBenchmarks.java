1c06cad1b1beb29427fea3c51c1b514249b1e2d1
siamc1 <siam.r.chowdhury@gmail.com>
2019-12-01 18:41:34 -0500

    Got rid of older code
    Older code segments that were previously commented out were removed.

--- src/sortingbenchmarks/SortingBenchmarks.java
+++ src/sortingbenchmarks/SortingBenchmarks.java
@@ -21,64 +21,6 @@ public class SortingBenchmarks{
         System.out.println("How many different sizes of arrays do you wish to try? ");
         int intMax = input.nextInt();
         
-        /*boolean boolBubble = true;
-        boolean boolSelection = true;
-        boolean boolInsertion = true;
-        boolean boolQuick = true;
-        int num = 10;
-        
-        
-        System.out.print("What is the most time it can take for a sorting algorithm to sort any given set of values?\t");
-        
-        
-        long bubbleTime = 0, selectionTime = 0, insertionTime = 0, quickTime = 0;
-        while(boolQuick && boolInsertion){
-            testCase = new int[num];
-            for (int i = 0; i < num; i++){
-                testCase[i] = r.nextInt(num);
-            }
-            if(boolBubble){
-                long startTimeBubble = System.nanoTime();
-                int[] bubbleSorted = BubbleSort(testCase);
-                long stopTimeBubble = System.nanoTime();
-                bubbleTime = stopTimeBubble - startTimeBubble;
-                System.out.println("Time required for Bubble sort of " + num + " integers: " + bubbleTime);
-            }
-            if(boolSelection){
-                long startTimeSelection = System.nanoTime();
-                int[] selectionSorted = SelectionSort(testCase);
-                long stopTimeSelection = System.nanoTime();
-                selectionTime = stopTimeSelection - startTimeSelection;
-                System.out.println("Time required for Selection sort of " + num + " integers: " + selectionTime);
-            }
-            if(boolInsertion){
-                long startTimeInsertion = System.nanoTime();
-                int[] insertionSorted = InsertionSort(testCase);
-                long stoptTimeInsertion = System.nanoTime();
-                insertionTime = stoptTimeInsertion - startTimeInsertion;
-                System.out.println("Time required for Insertion sort of " + num + " integers: " + insertionTime);
-            }
-            if(boolQuick){
-                long startTimeQuick = System.nanoTime();
-                int[] quickSorted = QuickSort(testCase, 0, num);
-                long stopTimeQuick = System.nanoTime();
-                quickTime = stopTimeQuick - startTimeQuick;
-                System.out.println("Time required for Quick sort of " + num + " integers: " + quickTime);
-            }
-            System.out.println("");
-            if(bubbleTime > intMax * Math.pow(10, 9)){
-                boolBubble = false;
-            }
-            if(selectionTime > intMax * Math.pow(10, 9)){
-                boolSelection = false;
-            }
-            if(insertionTime > intMax * Math.pow(10, 9)){
-                boolInsertion = false;
-            }
-            if(quickTime > intMax * Math.pow(10, 9)){
-                boolQuick = false;
-            }
-            num*=10;*/
         int num = 10;
         for ( int j = 1; j <= intMax; j++){
             testCase = new int[num];
@@ -115,68 +57,6 @@ public class SortingBenchmarks{
             num *= 10;
         }
     }
-        /*
-        int[] testCase;
-        
-        for ( int j = 1; j <= 10000; j *= 10){
-            int num = 10 * j;
-            testCase = new int[num];
-            for (int i = 0; i < num; i++){
-                testCase[i] = r.nextInt(num);
-            }
-        
-            long startTimeBubble = System.nanoTime();
-            int[] bubbleSorted = BubbleSort(testCase);
-            long stopTimeBubble = System.nanoTime();
-        
-            long startTimeSelection = System.nanoTime();
-            int[] selectionSorted = SelectionSort(testCase);
-            long stopTimeSelection = System.nanoTime();
-        
-            long startTimeInsertion = System.nanoTime();
-            int[] insertionSorted = InsertionSort(testCase);
-            long stoptTimeInsertion = System.nanoTime();
-            
-            long startTimeQuick = System.nanoTime();
-            int[] quickSorted = QuickSort(testCase, 0, num);
-            long stopTimeQuick = System.nanoTime();
-            
-            long bubbleTime = stopTimeBubble - startTimeBubble;
-            long selectionTime = stopTimeSelection - startTimeSelection;
-            long quickTime = stopTimeQuick - startTimeQuick;
-            long insertionTime = stoptTimeInsertion - startTimeInsertion;
-            
-            System.out.println("Time required for bubble sort of " + num + " Terms: " + bubbleTime);
-            System.out.println("Time required for selection sort of " + num + " Terms: " + selectionTime);
-            System.out.println("Time required for insertion sort of " + num + " Terms: " + insertionTime);
-            System.out.println("Time required for quick sort of " + num + " Terms: " + quickTime + "\n");
-        }
-        
-        System.out.println("\nBubble Sort has been eliminated for taking too long\nSelection Sort has been eliminated for taking too long\n");
-        
-        for ( int j = 100000; j < 100000000; j *= 10){
-            int num = 10 * j;
-            testCase = new int[num];
-            for (int i = 0; i < num; i++){
-                testCase[i] = r.nextInt(num);
-            }
-        
-            long startTimeQuick = System.nanoTime();
-            int[] quickSorted = QuickSort(testCase, 0, num - 1);
-            long stopTimeQuick = System.nanoTime();
-            long quickTime = stopTimeQuick - startTimeQuick;
-            System.out.println("Time required for quick sort of " + num + " Terms: " + quickTime);
-            
-            long startTimeInsertion = System.nanoTime();
-            int[] insertionSorted = InsertionSort(testCase);
-            long stoptTimeInsertion = System.nanoTime();
-            long insertionTime = stoptTimeInsertion - startTimeInsertion;
-            System.out.println("Time required for insertion sort of " + num + " Terms: " + insertionTime + "\n");  
-        }
-            
-        
-    }
-    */ 
     public static int[] BubbleSort(int[] values){
         int intTemp;
         for (int i = values.length - 1; i >= 0; i--){
